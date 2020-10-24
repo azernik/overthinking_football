6393e90a0ac6ada451a8bb70b4f2a70662f9c5da
adamscj14 <adamscj14@gmail.com>
2020-10-24 11:54:01 -0400

    Updated data and xfpts scripts

---
+++ fbref_scripts/pull_new_data_fbref.py
@@ -0,0 +1,321 @@
+import sys
+import pandas as pd
+import time
+from urllib.error import HTTPError
+import json
+import yaml
+import getopt
+
+accio_tables_string = "http://acciotables.herokuapp.com/?page_url="
+table_selector_string = "&content_selector_id=%23"
+
+
+
+def help(exit_num = 1):
+    print("""-----------------------------------------------------------------
+ARGUMENTS
+    -c => <yaml> Config File REQUIRED
+    -s => <string> string of the first gameweek to include in newly pulled data 
+                    REQUIRED unless override is specified, in which case pull all data
+    --override <boolean> pull all data OPTIONAL
+""")
+    sys.exit(exit_num)
+
+
+def main(argv): 
+    try: 
+        opts, args = getopt.getopt(sys.argv[1:], "c:s:", ['override']) 
+
+    except getopt.GetoptError:
+        print("Error: Incorrect usage of getopts flags!")
+        help() 
+
+    options_dict = dict(opts)
+    
+    ## Required arguments
+    try:
+        config_file = options_dict['-c']
+
+    except KeyError:
+        print("Error: One of your required arguments does not exist.")
+        help()
+
+    first_week_string = options_dict.get('-s', False)
+    override = options_dict.get('--override', False)
+    if override == '':
+        override = True
+
+    if not override:
+        if not first_week_string:
+            print("Error: Need to specify a first week string")
+            help()
+
+
+    print("Acceptable Inputs Given")
+
+    driver(config_file, first_week_string, override)
+
+def driver(config_file, first_week_string, override):
+
+    config_dict = yaml.load(open(config_file, 'r'), Loader=yaml.SafeLoader)
+
+    match_url_file = config_dict['match_url_file']
+    fbref_tables_dict = config_dict['fbref_tables']
+    team_id_file = config_dict['team_id_file']
+    name_conversion_json = config_dict['name_conversion_file']
+
+    ### Get the dict linking team id with team
+    team_id_dict = get_team_id_dict(team_id_file)
+
+    # iterate through list of matches in a season to get all the data for every player
+    full_table_dict = pull_match_data(match_url_file, team_id_dict, first_week_string, override)
+
+    # NEXT STEPS ARE TO CLEAN UP DATA
+
+    ## open name conversion dict
+    with open(name_conversion_json, 'r') as j_file:
+        name_conversion_dict = json.load(j_file)
+
+    check_players_to_change(name_conversion_dict, full_table_dict)
+
+    # clean the data and append to the master file
+    for table_type in ['summary', 'passing', 'passing_types', 'defense', 'possession', 'misc']:
+        
+        df = full_table_dict[table_type]
+        new_df = df.replace(name_conversion_dict)
+        convert_dates(new_df)
+
+        master_file = fbref_tables_dict[table_type]
+
+        if override:
+            new_df.to_csv(master_file)
+        else:
+            with open(master_file) as f:
+                f.write('\n')
+            new_df.to_csv(master_file, mode='a', header=False)
+
+
+
+
+
+
+
+
+
+def pull_match_data(match_url_file, team_id_dict, first_week_string, override):
+
+    start_matching = False
+    if override:
+        start_matching = True
+
+    count = 0  
+
+    ## saving each table individually
+    summary_table_df = None
+    passing_table_df = None
+    passing_types_table_df = None
+    defense_table_df = None
+    possession_table_df = None
+    misc_table_df = None
+
+    full_table_dict = {'summary': summary_table_df,
+                   'passing': passing_table_df,
+                   'passing_types': passing_types_table_df,
+                   'defense': defense_table_df,
+                   'possession': possession_table_df,
+                   'misc': misc_table_df}
+    
+    match_urls = open(match_url_file, 'r')
+    for match_url in match_urls:
+
+        match_url = match_url.strip()
+        match_url_list = match_url.split('/')
+
+        if not start_matching:
+            if match_url_list[-1] == first_week_string:
+                start_matching = True
+            else:
+                continue
+        count += 1
+        ## first identify teams playing
+        home_team, away_team, date = get_overall_match_info(match_url_list[-1], team_id_dict)
+        ## construct the base url to query
+        base_url = accio_tables_string + match_url + table_selector_string
+        for team in [home_team, away_team]:
+            team_id, team_string = team_id_dict[team]
+            # get the opponent
+            opponent_team_string = team_id_dict[home_team][1]
+            if team == home_team:
+                opponent_team_string = team_id_dict[away_team][1]
+            
+            for table_type in ['summary', 'passing', 'passing_types', 'defense', 'possession', 'misc']:
+                #print(table_type)
+                # don't want to overburden the system
+                time.sleep(2)
+                
+                full_table_df = full_table_dict[table_type]
+                #print(full_table_df)
+                table_id_string = 'stats_' + team_id + '_' + table_type
+                table_url = str(base_url + table_id_string)
+            
+                # get summary table
+                http_error = True
+                while http_error:
+                    try:
+                        team_table_df = pd.read_html(table_url, header=1)[0]
+                        http_error = False
+                    except HTTPError:
+                        # if there happens to be an html error, then wait 30 seconds and try again
+                        print('Html error! Waiting 30 seconds and trying again...')
+                        time.sleep(5)
+                
+                team_table_df.drop(team_table_df.tail(1).index,inplace=True)
+            
+                # add date column
+                team_table_df['Date'] = [date] * len(team_table_df)
+                team_table_df['Team'] = [team_string] * len(team_table_df)
+                team_table_df['Opponent'] = [opponent_team_string] * len(team_table_df)
+            
+                try:
+                    full_table_df = full_table_df.append(team_table_df, ignore_index = True)
+                except AttributeError:
+                    full_table_df = team_table_df
+                #if table_type == 'summary':
+                #        print(full_table_df)
+                full_table_dict[table_type] = full_table_df
+        print(count)
+    ## run some qc
+    if not start_matching:
+        print("Error: Never found match of interest")
+        help()
+
+    return full_table_dict
+
+def check_players_to_change(name_conversion_dict, full_table_dict):
+
+    # Find the players whose names need to be converted in the name conversion json
+    fantrax_file_19_20 = "/Users/adamscj/Documents/GitHub/overthinking_football/data/fantrax_data/fantrax.weekly.all_data_combined.no_double_gws.new_positions.csv"
+    fantrax_file_20_21 ="/Users/adamscj/Documents/GitHub/overthinking_football/data/fantrax_data/2020_2021_data/fantrax.weekly.no_double_gws.20_21.csv"
+
+    players_19_20 = list(pd.read_csv(fantrax_file_19_20)['Player'])
+    players_20_21 = list(pd.read_csv(fantrax_file_20_21)['Player'])
+    #print(players_19_20)
+    missing_player = []
+    summary_table = full_table_dict['summary']
+    player_count = -1
+    for player in summary_table['Player']:
+        player_count += 1
+        if player not in name_conversion_dict and player not in players_19_20 and player not in players_20_21:
+            player_pos = summary_table['Pos'][player_count]
+            print(player_pos)
+            if player_pos == 'GK':
+                continue
+            if player not in missing_player:
+                missing_player.append(player)
+
+    if len(missing_player) >= 1:
+        print(missing_player)
+        print("Exiting script now, too many missing players. Go back and fix!")
+        help()
+
+def get_overall_match_info(match_url, team_id_dict):
+    
+    teams_found = []
+    
+    # could be more efficient...but only dealing with 20 teams
+    for team in team_id_dict:
+        if team in match_url:
+            teams_found.append(team)
+    
+    # check that the appropriate number of teams have been identified in url
+    if len(teams_found) != 2:
+        print("Error: Improper number of teams found in url")
+        print(teams_found)
+        print(match_url_list)
+        return False
+    
+    ## id home and away teams and the date
+    
+    if '-'.join(teams_found) in match_url:
+        # implies the proper order
+        pass
+        
+    elif '-'.join(teams_found[::-1]) in match_url:
+        # swap the order
+        teams_found = teams_found[::-1]
+
+    else:
+        print("Error: url format assumptions are wrong")
+        return False
+    
+    # next get the date
+    date = get_date_from_url(teams_found, match_url)
+    
+    return teams_found + [date]
+
+def get_date_from_url(teams_found, match_url):
+    
+    # I'm assuming the following format:
+    # */HOMETEAM-AWAYTEAM-MONTH-DAY-YEAR-LEAGUE
+    # e.g */Aston-Villa-Manchester-United-July-9-2020-Premier-League
+    
+    teams_string_length = len(teams_found[0]) + len(teams_found[1]) + 2
+    date_and_league_string = match_url[teams_string_length:]
+    date_list = date_and_league_string.strip().split('-')[0:3]
+    
+    date = '-'.join(date_list)
+    
+    return date
+
+
+def get_team_id_dict(team_id_file):
+    
+    team_id_dict = {}
+
+    with open(team_id_file, 'r') as team_id_csv:
+        for line in team_id_csv:
+            team, id_string, table_team_string = line.strip().split(',')
+            team_id_dict[team] = [id_string, table_team_string]
+
+    return team_id_dict
+
+def convert_dates(df):
+    converted_date_column = []
+    for date in df['Date']:
+
+        new_date = date.replace('2019', '19')
+        new_date = new_date.replace('2020', '20')
+
+        new_date = new_date.replace('-1-', '-01-')
+        new_date = new_date.replace('-2-', '-02-')
+        new_date = new_date.replace('-3-', '-03-')
+        new_date = new_date.replace('-4-', '-04-')
+        new_date = new_date.replace('-5-', '-05-')
+        new_date = new_date.replace('-6-', '-06-')
+        new_date = new_date.replace('-7-', '-07-')
+        new_date = new_date.replace('-8-', '-08-')
+        new_date = new_date.replace('-9-', '-09-')
+
+        new_date = new_date.replace('January', '01')
+        new_date = new_date.replace('February', '02')
+        new_date = new_date.replace('March', '03')
+        new_date = new_date.replace('April', '04')
+        new_date = new_date.replace('May', '05')
+        new_date = new_date.replace('June', '06')
+        new_date = new_date.replace('July', '07')
+        new_date = new_date.replace('August', '08')
+        new_date = new_date.replace('September', '09')
+        new_date = new_date.replace('October', '10')
+        new_date = new_date.replace('November', '11')
+        new_date = new_date.replace('December', '12')
+
+        new_date = new_date.replace('-', '/')
+
+        converted_date_column.append(new_date)
+    
+    df['Date'] = converted_date_column
+    
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ pWAS_scripts/war_calculations_cleaned.py
@@ -0,0 +1,329 @@
+#!/usr/bin/Python
+
+# Created by: Christopher J Adams 9/29/2018
+# 
+
+###############################################################################
+###
+### This script calculates xfpts given the weekly game results
+###
+###############################################################################
+
+#import cProfile
+import sys
+import pandas as pd
+import numpy as np
+import random
+import cProfile
+import time
+import getopt
+
+def help(exit_code = 1):
+    print("""-----------------------------------------------------------------
+ARGUMENTS
+    -w => <csv> weekly xFpts REQUIRED
+    -t => <csv> season-long xfpts REQUIRED
+    -o => <path> name of output file REQUIRED
+""")
+    sys.exit(exit_code)
+###############################################################################
+###########################  COLLECT AND CHECK ARGUMENTS  #####################
+###############################################################################
+
+
+## MAIN ##
+
+def main(argv): 
+    pd.set_option('display.max_colwidth', 100)
+    try: 
+        opts, args = getopt.getopt(sys.argv[1:], "w:t:o:")
+    except getopt.GetoptError:
+        print("Error: Incorrect usage of getopts flags!")
+        help() 
+    
+    options_dict = dict(opts)
+
+    ## Required arguments
+    try:
+        weekly_xfpts_csv = options_dict['-w']
+        season_long_xfpts_csv = options_dict['-t']
+        output_file = options_dict['-o']
+    except KeyError:
+        print("Error: One of your required arguments does not exist.")
+        help()
+
+    ## Optional Arguments
+    #minute_requirement = options_dict.get('-m', 60)
+
+    print("Acceptable Inputs Given")
+
+    driver(weekly_xfpts_csv, season_long_xfpts_csv, output_file)
+
+
+def driver(weekly_xfpts_csv, season_long_xfpts_csv, output_file):
+
+    num_sample = 500000
+
+    xfpts_df = pd.read_csv(season_long_xfpts_csv)
+    weekly_xfpts_df = pd.read_csv(weekly_xfpts_csv)
+
+    fwd_group_dict = {1: [40, 35, 55],
+                      2: [50, 40, 60],
+                      3: [40, 35, 55]}
+
+    mid_group_dict = {1: [50, 45, 65],
+                      2: [60, 50, 80],
+                      3: [55, 45, 70]}
+
+    def_group_dict = {1: [40, 35, 55],
+                      2: [50, 40, 60],
+                      3: [50, 40, 60]}
+
+    player_pwas_dict = {}
+    for group in list(fwd_group_dict.keys()):
+
+        ## FWDS
+        fwd_total_starters, fwd_ww_start, fwd_ww_end = fwd_group_dict[group]
+        fwd_starter_game_scores, fwd_ww_game_scores = get_starters_and_ww("F", xfpts_df, weekly_xfpts_df, fwd_total_starters, fwd_ww_start, fwd_ww_end)
+        
+        ## MIDS
+        mid_total_starters, mid_ww_start, mid_ww_end = mid_group_dict[group]
+        mid_starter_game_scores, mid_ww_game_scores = get_starters_and_ww("M", xfpts_df, weekly_xfpts_df, mid_total_starters, mid_ww_start, mid_ww_end)
+        
+        ## DEF
+        def_total_starters, def_ww_start, def_ww_end = def_group_dict[group]
+        def_starter_game_scores, def_ww_game_scores = get_starters_and_ww("F", xfpts_df, weekly_xfpts_df, def_total_starters, def_ww_start, def_ww_end)
+        
+        starter_score_dict = {"D": def_starter_game_scores,
+                              "M": mid_starter_game_scores,
+                              "F": fwd_starter_game_scores}
+        ww_score_dict = {"D": def_ww_game_scores,
+                         "M": mid_ww_game_scores,
+                         "F": fwd_ww_game_scores}
+        ### Average Starter team sampling
+
+        starter_avg_game = []
+        ww_def_avg_game = []
+        ww_mid_avg_game = []
+        ww_fwd_avg_game = []
+        start = time.time()
+    
+        formation_list = [[3,4,3], [3,5,2], [4,4,2], [4,3,3]]
+
+        for formation in formation_list:
+
+            def_formation_count, mid_formation_count, fwd_formation_count = formation
+
+            ## first get the average game with all starters
+            starter_lineup_avg_game = get_game_results(starter_score_dict, formation, num_sample)
+            starter_avg_game = starter_avg_game + starter_lineup_avg_game
+
+            ## Average games for one player missing at each position
+            # DEF
+            def_missing_formation = formation[:]
+            def_missing_formation[0] -= 1
+            ww_def_included_avg_game = get_game_results(starter_score_dict, def_missing_formation, num_sample, ww_score_dict['D'])
+            ww_def_avg_game = ww_def_avg_game + ww_def_included_avg_game
+
+            # MID
+            mid_missing_formation = formation[:]
+            mid_missing_formation[0] -= 1
+            ww_mid_included_avg_game = get_game_results(starter_score_dict, mid_missing_formation, num_sample, ww_score_dict['M'])
+            ww_mid_avg_game = ww_mid_avg_game + ww_mid_included_avg_game
+
+            # FWD
+            fwd_missing_formation = formation[:]
+            fwd_missing_formation[0] -= 1
+            ww_fwd_included_avg_game = get_game_results(starter_score_dict, fwd_missing_formation, num_sample, ww_score_dict['F'])
+            ww_fwd_avg_game = ww_fwd_avg_game + ww_fwd_included_avg_game
+
+        # shuffle the avg game data and calculate the ww_pwas values
+        random.shuffle(starter_avg_game)
+
+        def_ww_win_percentage = get_win_percentage(ww_def_avg_game, starter_avg_game)
+        def_ww_war = def_ww_win_percentage - 0.5
+        print('Mean def ww game score: ', np.mean(ww_def_avg_game))
+        print('std def ww game score: ', np.std(ww_def_avg_game))
+        print("DEF avg war lost: ", def_ww_war)
+
+        mid_ww_win_percentage = get_win_percentage(ww_mid_avg_game, starter_avg_game)
+        mid_ww_war = mid_ww_win_percentage - 0.5
+        print('Mean mid ww game score: ', np.mean(ww_mid_avg_game))
+        print('std mid ww game score: ', np.std(ww_mid_avg_game))
+        print("MID avg war lost: ", mid_ww_war)
+
+        fwd_ww_win_percentage = get_win_percentage(ww_fwd_avg_game, starter_avg_game)
+        fwd_ww_war = fwd_ww_win_percentage - 0.5
+        print('Mean fwd ww game score: ', np.mean(ww_fwd_avg_game))
+        print('std fwd ww game score: ', np.std(ww_fwd_avg_game))
+        print("FWD avg war lost: ", fwd_ww_war)
+
+        ## Next cycle through each player
+        ## For all players SAMPLING procedure
+        player_dict = {}
+        for index, row in xfpts_df.iterrows():
+            print(index)
+            
+            sim_games = []
+            player = row['Player']
+            pos = row['Position']
+            games = row['n']
+            #print(player)
+            player_weekly_pts = weekly_xfpts_df.loc[(weekly_xfpts_df['Player'] == player)]['xFpts']
+            if player_weekly_pts.empty:
+                print(player + " is missing from weekly pts")
+                continue
+            
+            for formation in formation_list:
+                def_formation_count, mid_formation_count, fwd_formation_count = formation
+                def_count = def_formation_count
+                mid_count = mid_formation_count
+                fwd_count = fwd_formation_count
+                if pos == 'D':
+                    def_count -= 1
+                    ww_pwas = def_ww_war
+                elif pos == 'M':
+                    mid_count -= 1
+                    ww_pwas = mid_ww_war
+                elif pos == 'F':
+                    fwd_count -= 1
+                    ww_pwas = fwd_ww_war
+                
+                def_index_pos = 0
+                mid_index_pos = 0
+                fwd_index_pos = 0
+
+                player_samples = list(player_weekly_pts.sample(n = num_sample, replace = True))
+                def_samples = list(def_starter_game_scores.sample(n=(def_count * num_sample), replace = True))
+                mid_samples = list(mid_starter_game_scores.sample(n=(mid_count * num_sample), replace = True))
+                fwd_samples = list(fwd_starter_game_scores.sample(n=(fwd_count * num_sample), replace = True))
+                 
+                for sample in range(num_sample):
+                    def_index_2 = def_index_pos + def_count
+                    mid_index_2 = mid_index_pos + mid_count
+                    fwd_index_2 = fwd_index_pos + fwd_count
+                    total_pts = 0
+                    def_pts = def_samples[def_index_pos:def_index_2]
+                    mid_pts = mid_samples[mid_index_pos:mid_index_2]
+                    fwd_pts = fwd_samples[fwd_index_pos:fwd_index_2]
+                    player_sample = player_samples[sample]
+
+                    all_pts = def_pts + mid_pts + fwd_pts + [player_sample]
+                    for pts in all_pts:
+                        total_pts += pts
+
+                    def_index_pos = def_index_2
+                    mid_index_pos = mid_index_2
+                    fwd_index_pos = fwd_index_2
+
+                    sim_games.append(total_pts)
+
+            sim_games_mean = np.mean(sim_games)
+            sim_games_sd = np.std(sim_games)
+            
+            win_percentage = get_win_percentage(sim_games, starter_avg_game)
+            pwas = win_percentage - 0.5
+            games_missed = 38 - games
+            try:
+                player_dict[player][3] += pwas
+                player_dict[player][4] += ww_pwas
+            except KeyError:
+                player_dict[player] = [pos, games, games_missed, pwas, ww_pwas]
+
+    ## Finally clean up player dict
+    final_player_dict = {}
+    for player in player_dict:
+        total_groups = len(list(fwd_group_dict.keys()))
+        pos, games, games_missed, pwas, ww_pwas = player_dict[player]
+        avg_pwas = pwas / float(total_groups)
+        avg_ww_pwas = ww_pwas / float(total_groups)
+        pwar = avg_pwas - avg_ww_pwas
+        final_player_dict[player] = [pos, \
+                                     games, \
+                                     games_missed, \
+                                     avg_pwas, \
+                                     avg_ww_pwas, \
+                                     pwar,
+                                     pwar * float(games)]
+
+    ## convert player dict to pandas df
+    df = pd.DataFrame.from_dict(final_player_dict, orient='index', columns = ['Pos', 'games_played', 'games_missed', 'pWAS', 'ww_pWAS', 'pWAR', 'WAR'])
+
+    df = df.sort_values(by = 'WAR', ascending = False)
+
+    df.to_csv(output_file)
+
+
+
+def get_game_results(starter_score_dict, pos_counts, num_sample, ww_added_df = None):
+
+    def_count, mid_count, fwd_count = pos_counts
+
+    def_samples = list(starter_score_dict['D'].sample(n=def_count*num_sample, replace = True))
+    mid_samples = list(starter_score_dict['M'].sample(n=mid_count*num_sample, replace = True))
+    fwd_samples = list(starter_score_dict['F'].sample(n=fwd_count*num_sample, replace = True))
+    if ww_added_df is not None:
+        ww_samples = list(ww_added_df.sample(n=num_sample, replace = True))
+
+    def_index_pos = 0
+    mid_index_pos = 0
+    fwd_index_pos = 0
+
+    game_list = []
+
+    for sample in range(num_sample):
+        def_index_2 = def_index_pos + def_count
+        mid_index_2 = mid_index_pos + mid_count
+        fwd_index_2 = fwd_index_pos + fwd_count
+        total_pts = 0
+        def_pts = def_samples[def_index_pos:def_index_2]
+        mid_pts = mid_samples[mid_index_pos:mid_index_2]
+        fwd_pts = fwd_samples[fwd_index_pos:fwd_index_2]
+        all_pts = def_pts + mid_pts + fwd_pts
+        for pts in all_pts:
+            total_pts += pts
+        if ww_added_df is not None:
+            ww_pts = ww_samples[sample]
+            total_pts += ww_pts
+
+
+        def_index_pos = def_index_2
+        mid_index_pos = mid_index_2
+        fwd_index_pos = fwd_index_2
+
+        game_list.append(total_pts)
+    
+    return game_list   
+
+
+
+def get_starters_and_ww(pos, xfpts_df, weekly_xfpts_df, total_starters, ww_start, ww_end):
+
+    starters = list(xfpts_df.loc[(xfpts_df['Position'] == pos) & (xfpts_df['n'] >= 10)].sort_values(by = 'mean_xFpts', ascending = False)[0:total_starters]['Player'])
+    print(pos + " starters: \n", starters)
+    starter_game_scores = weekly_xfpts_df.loc[(weekly_xfpts_df['Player'].isin(starters))]['xFpts']
+    ww_players = list(xfpts_df.loc[(xfpts_df['Position'] == pos) & (xfpts_df['n'] >= 5)].sort_values(by = 'mean_xFpts', ascending = False)[ww_start:ww_end]['Player'])
+    print(pos + " ww: \n", ww_players)
+    ww_game_scores = weekly_xfpts_df.loc[(weekly_xfpts_df['Player'].isin(ww_players))]['xFpts']
+
+    return starter_game_scores, ww_game_scores
+
+
+def get_win_percentage(game_list_1, game_list_2):
+    
+    num_matchups = len(game_list_1)
+
+    diff = np.array(game_list_1) - np.array(game_list_2)
+    
+    win_count = 0
+    for d in diff:
+        if d > 0:
+            win_count += 1
+    win_percentage = win_count / num_matchups
+
+    return win_percentage
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+ 
